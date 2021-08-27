800c2df5b0fa3020b7cb1195ef50468034f74e44
Shahbaj Ali <ali@anviam.com>
2021-08-27 15:12:59 +0530

    App logo, changes texts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,6 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
   end
 end
