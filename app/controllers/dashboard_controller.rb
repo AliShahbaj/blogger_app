2383de8de2fa4e41452b333043fa88af4c7f220a
Shahbaj Ali <ali@anviam.com>
2021-08-27 15:46:27 +0530

    Fixed redirect issue

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,6 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = Post.all.order("created_at DESC")
+    @posts = Post.all.order("updated_at DESC")
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,8 +39,8 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { render :show, status: :ok, location: @post }
+        format.html { redirect_to root_path, notice: 'Post was successfully updated.' }
+        format.json { render root_path, status: :ok, location: @post }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
